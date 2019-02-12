525d540ee77fdd4af764410c0c2e82bb980e5bb0
kristin-dean <47224694+kristin-dean@users.noreply.github.com>
2019-02-11 20:11:17 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,7 +1,7 @@
 var correct1 = function()
 {
   document.getElementById("feedback1").innerHTML = "Correct!";
-  document.getElementById("feedback1").font.color = "#008000";
+  document.getElementById("feedback1").style.color = "#008000";
 }
 
 var incorrect1 = function()
@@ -13,44 +13,44 @@ var incorrect1 = function()
 var correct2 = function()
 {
   document.getElementById("feedback2").innerHTML = "Correct!";
-  document.getElementById("feedback2").font.color = "#008000";
+  document.getElementById("feedback2").style.color = "#008000";
 }
 
 var incorrect2 = function()
 {
   document.getElementById("feedback2").innerHTML = "Incorrect...";
-  document.getElementById("feedback2").font.color = "#FF0000";
+  document.getElementById("feedback2").style.color = "#ff0000";
 }
 var correct3 = function()
 {
   document.getElementById("feedback3").innerHTML = "Correct!";
-  document.getElementById("feedback3").font.color = "#008000";
+  document.getElementById("feedback3").stle.color = "#006400";
 }
 
 var incorrect3 = function()
 {
   document.getElementById("feedback3").innerHTML = "Incorrect...";
-  document.getElementById("feedback3").font.color = "#FF0000";
+  document.getElementById("feedback3").style.color = "#ff0000";
 }
 var correct4 = function()
 {
   document.getElementById("feedback4").innerHTML = "Correct!";
-  document.getElementById("feedback4").font.color = "#008000";
+  document.getElementById("feedback4").style.color = "#006400";
 }
 
 var incorrect4 = function()
 {
   document.getElementById("feedback4").innerHTML = "Incorrect...";
-  document.getElementById("feedback4").font.color = "#FF0000";
+  document.getElementById("feedback4").style.color = "#ff0000";
 }
 var correct5 = function()
 {
   document.getElementById("feedback5").innerHTML = "Correct!";
-  document.getElementById("feedback5").font.color = "#008000";
+  document.getElementById("feedback5").style.color = "#006400";
 }
 
 var incorrect5 = function()
 {
   document.getElementById("feedback5").innerHTML = "Incorrect...";
-  document.getElementById("feedback5").font.color = "#FF0000";
+  document.getElementById("feedback5").style.color = "#ff0000";
 }
