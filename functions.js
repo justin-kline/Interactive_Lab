627557252bc9b5ad4422f11eca7823d429d34608
kristin-dean <47224694+kristin-dean@users.noreply.github.com>
2019-02-11 20:33:58 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -24,6 +24,8 @@ var correct2 = function()
   document.getElementById("feedback2").style.color = "#006400";
   document.getElementById("q2buttonT").disabled = true;
   document.getElementById("q2buttonF").disabled = true;
+  document.getElementById("q3buttonT").disabled = false;
+  document.getElementById("q3buttonF").disabled = false;
 }
 
 var incorrect2 = function()
@@ -32,37 +34,59 @@ var incorrect2 = function()
   document.getElementById("feedback2").style.color = "#ff0000";
   document.getElementById("q2buttonT").disabled = true;
   document.getElementById("q2buttonF").disabled = true;
+  document.getElementById("q3buttonT").disabled = false;
+  document.getElementById("q3buttonF").disabled = false;
 }
 var correct3 = function()
 {
   document.getElementById("feedback3").innerHTML = "Correct!";
   document.getElementById("feedback3").stle.color = "#006400";
+  document.getElementById("q3buttonT").disabled = true;
+  document.getElementById("q3buttonF").disabled = true;
+  document.getElementById("q4buttonT").disabled = false;
+  document.getElementById("q4buttonF").disabled = false;
 }
 
 var incorrect3 = function()
 {
   document.getElementById("feedback3").innerHTML = "Incorrect...";
   document.getElementById("feedback3").style.color = "#ff0000";
+  document.getElementById("q3buttonT").disabled = true;
+  document.getElementById("q3buttonF").disabled = true;
+  document.getElementById("q4buttonT").disabled = false;
+  document.getElementById("q4buttonF").disabled = false;
 }
 var correct4 = function()
 {
   document.getElementById("feedback4").innerHTML = "Correct!";
   document.getElementById("feedback4").style.color = "#006400";
+  document.getElementById("q4buttonT").disabled = true;
+  document.getElementById("q4buttonF").disabled = true;
+  document.getElementById("q5buttonT").disabled = false;
+  document.getElementById("q5buttonF").disabled = false;
 }
 
 var incorrect4 = function()
 {
   document.getElementById("feedback4").innerHTML = "Incorrect...";
   document.getElementById("feedback4").style.color = "#ff0000";
+  document.getElementById("q4buttonT").disabled = true;
+  document.getElementById("q4buttonF").disabled = true;
+  document.getElementById("q5buttonT").disabled = false;
+  document.getElementById("q5buttonF").disabled = false;
 }
 var correct5 = function()
 {
   document.getElementById("feedback5").innerHTML = "Correct!";
   document.getElementById("feedback5").style.color = "#006400";
+  document.getElementById("q5buttonT").disabled = true;
+  document.getElementById("q5buttonF").disabled = true;
 }
 
 var incorrect5 = function()
 {
   document.getElementById("feedback5").innerHTML = "Incorrect...";
   document.getElementById("feedback5").style.color = "#ff0000";
+  document.getElementById("q5buttonT").disabled = true;
+  document.getElementById("q5buttonF").disabled = true;
 }
