3333814b335f59b69c6ff70fe9a05bad72737a10
kristin-dean <47224694+kristin-dean@users.noreply.github.com>
2019-02-11 19:56:09 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -9,3 +9,48 @@ var incorrect1 = function()
   document.getElementById("feedback1").innerHTML = "Incorrect...";
   document.getElementById("feedback1").font.color = "#FF0000";
 }
+
+var correct2 = function()
+{
+  document.getElementById("feedback2").innerHTML = "Correct!";
+  document.getElementById("feedback2").font.color = "#008000";
+}
+
+var incorrect2 = function()
+{
+  document.getElementById("feedback2").innerHTML = "Incorrect...";
+  document.getElementById("feedback2").font.color = "#FF0000";
+}
+var correct3 = function()
+{
+  document.getElementById("feedback2").innerHTML = "Correct!";
+  document.getElementById("feedback2").font.color = "#008000";
+}
+
+var incorrect3 = function()
+{
+  document.getElementById("feedback2").innerHTML = "Incorrect...";
+  document.getElementById("feedback2").font.color = "#FF0000";
+}
+var correct4 = function()
+{
+  document.getElementById("feedback2").innerHTML = "Correct!";
+  document.getElementById("feedback2").font.color = "#008000";
+}
+
+var incorrect4 = function()
+{
+  document.getElementById("feedback2").innerHTML = "Incorrect...";
+  document.getElementById("feedback2").font.color = "#FF0000";
+}
+var correct5 = function()
+{
+  document.getElementById("feedback2").innerHTML = "Correct!";
+  document.getElementById("feedback2").font.color = "#008000";
+}
+
+var incorrect5 = function()
+{
+  document.getElementById("feedback2").innerHTML = "Incorrect...";
+  document.getElementById("feedback2").font.color = "#FF0000";
+}
