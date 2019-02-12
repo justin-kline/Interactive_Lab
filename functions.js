b96308b1fd52bb1fa868c336b809b4fb53516e46
kristin-dean <47224694+kristin-dean@users.noreply.github.com>
2019-02-11 19:46:37 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -1,9 +1,13 @@
 var correct1 = function()
 {
+  p = document.getElementById("feedback1");
+  p.setAttribute(color="green")
   document.getElementById("feedback1").innerHTML = "Correct!";
 }
 
 var incorrect1 = function()
 {
+  p = document.getElementById("feedback1");
+  p.setAttribute(color="red")
   document.getElementById("feedback1").innerHTML = "Incorrect...";
 }
