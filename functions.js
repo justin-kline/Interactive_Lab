12362d2d1e57fe193a4908b37084d230c4745d67
kristin-dean <47224694+kristin-dean@users.noreply.github.com>
2019-02-11 20:43:08 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -21,7 +21,7 @@ var incorrect1 = function()
 var correct2 = function()
 {
   document.getElementById("feedback2").innerHTML = "Correct!";
-  document.getElementById("feedback2").style.color = "#006400";
+  document.getElementById("feedback2").style.color = "#6b8e23";
   document.getElementById("q2buttonT").disabled = true;
   document.getElementById("q2buttonF").disabled = true;
   document.getElementById("q3buttonT").disabled = false;
@@ -40,7 +40,7 @@ var incorrect2 = function()
 var correct3 = function()
 {
   document.getElementById("feedback3").innerHTML = "Correct!";
-  document.getElementById("feedback3").stle.color = "#006400";
+  document.getElementById("feedback3").stle.color = "#6b8e23";
   document.getElementById("q3buttonT").disabled = true;
   document.getElementById("q3buttonF").disabled = true;
   document.getElementById("q4buttonT").disabled = false;
@@ -59,7 +59,7 @@ var incorrect3 = function()
 var correct4 = function()
 {
   document.getElementById("feedback4").innerHTML = "Correct!";
-  document.getElementById("feedback4").style.color = "#006400";
+  document.getElementById("feedback4").style.color = "#6b8e23";
   document.getElementById("q4buttonT").disabled = true;
   document.getElementById("q4buttonF").disabled = true;
   document.getElementById("q5buttonT").disabled = false;
@@ -78,7 +78,7 @@ var incorrect4 = function()
 var correct5 = function()
 {
   document.getElementById("feedback5").innerHTML = "Correct!";
-  document.getElementById("feedback5").style.color = "#006400";
+  document.getElementById("feedback5").style.color = "#6b8e23";
   document.getElementById("q5buttonT").disabled = true;
   document.getElementById("q5buttonF").disabled = true;
 }
